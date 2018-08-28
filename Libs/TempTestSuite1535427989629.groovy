d16ac2d340c4e494faa8c2c1e094581da9372f50
BHEEMARAJ UDAPUDI <urbraju@gmail.com>
2018-08-27 22:58:15 -0500

    Add files via upload
    KatalonDemo Files

---
+++ Libs/TempTestSuite1535427989629.groovy
@@ -0,0 +1,35 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/DemoTestSuite')
+
+suiteProperties.put('name', 'DemoTestSuite')
+
+suiteProperties.put('description', '')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\139741\\Katalon Studio\\DemoProjectKatalon\\Reports\\DemoTestSuite\\20180827_224629\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/DemoTestSuite', suiteProperties, [new TestCaseBinding('Test Cases/DemoTest1', 'Test Cases/DemoTest1',  null)])
