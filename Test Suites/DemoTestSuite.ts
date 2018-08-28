af5dc0e1eb3f9d20f870c0ae68c1254cc8b72157
BHEEMARAJ UDAPUDI <urbraju@gmail.com>
2018-08-27 23:10:38 -0500

    Add files via upload
    KataloN demo

---
+++ Test Suites/DemoTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-27T22:48:37</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <testSuiteGuid>771bda23-a826-4303-a028-4f70469adb09</testSuiteGuid>
+   <testCaseLink>
+      <guid>f5ff2d9d-14f0-4a68-8714-e24f02bd2023</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoTest1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/DemoTestSuite2.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoTestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3dc3721a-7733-437a-92be-864eeae0de7d</testSuiteGuid>
+   <testCaseLink>
+      <guid>4e080248-e67f-4fa5-b614-4b1f7a4887eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoTest1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
